80fa46720483aa32f791185127d0d1b64913ff40
Iruobe <Iruobe20@gmail.com>
2024-01-30 12:07:49 +0000

    Created link textview for login and register page as well as  added functionality for activity change when clicked

--- app/src/main/java/com/example/handyproject/MainActivity.java
+++ app/src/main/java/com/example/handyproject/MainActivity.java
@@ -4,16 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button SendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //main
-        //setContentView(R.layout.activity_customer_registration); //customer registration page
-        Intent intent = new Intent(this, CustomerRegistration.class);
-        startActivity(intent);
+
+
+        //Lec Added code in the lab to display the registration page
+//        Intent intent = new Intent(this, CustomerRegistration.class);
+//        startActivity(intent);
+
+        //OnClickLister for registration textview
+        TextView textView = findViewById(R.id.RegisterLinkTextView);
+        textView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, CustomerRegistration.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
